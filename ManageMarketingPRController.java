d3a2f0688515f8b632082f09a86bb2a2aaf5b120
razia9090 <2210915@iub.edu.bd>
2025-08-01 12:11:46 +0600

    razia

---
+++ ManageMarketingPRController.java
@@ -0,0 +1,39 @@
+package com.example.fivestarthotel.GeneralManager;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ManageMarketingPRController
+{
+    @javafx.fxml.FXML
+    private TextField budgetAmountTF;
+    @javafx.fxml.FXML
+    private TextField targetAudienceTF;
+    @javafx.fxml.FXML
+    private Label campaignLaunchedSuccessfullyL;
+    @javafx.fxml.FXML
+    private TextField campaignNameTF;
+    @javafx.fxml.FXML
+    private Label pleaseFillAllFieldsL;
+    @javafx.fxml.FXML
+    private ComboBox statusCB;
+    @javafx.fxml.FXML
+    private DatePicker dateDP;
+    @javafx.fxml.FXML
+    private Label manageHotelMarketingL;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void returnToDashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void lanuchCampaignOA(ActionEvent actionEvent) {
+    }
+}
